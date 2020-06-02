27452b7121a145519c47dee08f44bd5585145c60
thiatefa <47690145+thiatefa@users.noreply.github.com>
2020-06-02 12:15:34 +0200

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,11 @@
 // Update with your config settings.
 
-const pg = require('pg');
 
+const pg = require('pg');
+require("dotenv").config({ path: `${__dirname}/.env` });
+console.log( process.env.DATABASE_URL);
 module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL
 };
+
